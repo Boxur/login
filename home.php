aa6c6d88b1b8a003df8020e30e854966df2da901
Boxur <burekgamingpl@wp.pl>
2024-03-05 07:51:34 +0100

    update i guess
    added a head part

--- home.php
+++ home.php
@@ -22,6 +22,10 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8" />
+        <title>simple login</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+            
         <link rel="stylesheet" href="style.css" type="text/css" />
     </head>
     <body>
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE HTML>
 <html>
     <head>
-
+        <meta charset="utf-8" />
+        <title>simple login</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+            
+        <link rel="stylesheet" href="style.css" type="text/css" />
     </head>
     <body>
         <a href="login.php">LOGIN</a>
